f60885a923f2b9b31c7fd200e89e25e881a83739
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-19 17:11:52 +0200

    Init react app with burger list

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="nav">Le bon burger</nav>
+
+      <div className="container">
+        <div className="burger-card">
+          <img
+            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
+            alt="Kawaii Burger"
+          />
+          <p>Kawaii Burger</p>
+          <p>6€90</p>
+        </div>
+        <div className="burger-card">
+          <img
+            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
+            alt="Kawaii Burger"
+          />
+          <p>Kawaii Burger</p>
+          <p>6€90</p>
+        </div>
+        <div className="burger-card">
+          <img
+            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
+            alt="Kawaii Burger"
+          />
+          <p>Kawaii Burger</p>
+          <p>6€90</p>
+        </div>
+        <div className="burger-card">
+          <img
+            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
+            alt="Kawaii Burger"
+          />
+          <p>Kawaii Burger</p>
+          <p>6€90</p>
+        </div>
+        <div className="burger-card">
+          <img
+            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
+            alt="Kawaii Burger"
+          />
+          <p>Kawaii Burger</p>
+          <p>6€90</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
