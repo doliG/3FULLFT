e8d1c57d5dac4de4a8db619170926822dd058fea
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-19 13:03:00 +0200

    Partial correction of exercice 3

--- rappels_js/03.js
+++ rappels_js/03.js
@@ -1,18 +1,18 @@
 // <Ne pas toucher>
 function glutenOrNotGluten(withGluten) {
-    if (withGluten) {
-        return 'gluten'
-    } else {
-        return 'sans-gluten'
-    }
+  if (withGluten) {
+    return "gluten"
+  } else {
+    return "sans-gluten"
+  }
 }
 
 function makeKebab(kebab, withGluten) {
-    if (withGluten) {
-        return kebab + 'gluten'
-    } else {
-        return kebab + 'sans-gluten'
-    }
+  if (withGluten) {
+    return kebab + "gluten"
+  } else {
+    return kebab + "sans-gluten"
+  }
 }
 // </Ne pas toucher>
 
@@ -21,24 +21,32 @@ function makeKebab(kebab, withGluten) {
 // Un kebab classique à cette forme: "pain salade tomate oignon gluten".
 // Complétez la fonction suivante.
 // 💡 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
-function makeManyKebabs() {
-    // ...
-}
+const makeManyKebabs = () =>
+  new Array(10).fill("pain salade tomate oignon gluten")
 
 // Le chef s'est planté, il devait faire en plus trois kebab sans gluten.
 // Récupérez les kebab et ajoutez en trois nouveaux sans gluten "pain salade tomate oignon sans-gluten"
 // A la fin vous devez avoir une liste de 13 kebab, dont trois sans gluten.
 function makeManyVariousKebabs() {
-    const kebabs = makeManyKebabs()
-    // ...
+  //   const kebabs = makeManyKebabs()
+  //   const kebabWithoutGluten = new Array(3).fill(
+  //     "pain salade tomate oignon sans-gluten"
+  //   )
+  //   return [...kebabs, ...kebebWithoutGluten]
+
+  const kebabs = makeManyKebabs()
+  const kebabWithoutGluten = new Array(3).fill(
+    "pain salade tomate oignon sans-gluten"
+  )
+  const newKebabs = kebabs.concat(kebabWithoutGluten)
+  return newKebabs
 }
 
 // Des parisiens sont arrivés ! Récupérez tous les kebab sans gluten, ils ont faim.
 // Récupérez dans la liste des kebabs uniquement les kebab avec la mention 'sans-gluten'
 // 💡 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-function getWithoutGluten(kebabs) {
-    // ...
-}
+const getWithoutGluten = kebabs =>
+  kebabs.filter(kebab => kebab.includes("sans-gluten"))
 
 // 10 lyonnais sont arrivé. Moins chochottes, ils sont ok pour prendre les kebab avec gluten,
 // mais pas sans sauce ! Ajoutez de la sauce blanche harissa et servez leur!
@@ -47,7 +55,7 @@ function getWithoutGluten(kebabs) {
 // à la fin de la string représentant le kebab.
 // Completer la fonction ci-dessous
 function getWithSauce(kebabs, sauce) {
-    // ...
+  // ...
 }
 
 module.exports = { makeManyKebabs, makeManyVariousKebabs, getWithoutGluten }
