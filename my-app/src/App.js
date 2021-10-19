25c28514b784ca44f340b762ddbde69061e27992
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-19 17:28:49 +0200

    Display burgers from array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,52 @@
 import "./App.css"
 
 function App() {
+  const burgers = [
+    {
+      name: "L'instagrammable",
+      image:
+        "https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg",
+      price: "10€90",
+    },
+    {
+      name: "Kawaii Burger",
+      image:
+        "https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg",
+      price: "6€90",
+    },
+    {
+      name: "Kawaii Burger",
+      image:
+        "https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg",
+      price: "6€90",
+    },
+    {
+      name: "Kawaii Burger",
+      image:
+        "https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg",
+      price: "6€90",
+    },
+    {
+      name: "Kawaii Burger",
+      image:
+        "https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg",
+      price: "6€90",
+    },
+  ]
+
+  const burgerList = burgers.map(burger => (
+    <div className="burger-card">
+      <img src={burger.image} alt="Kawaii Burger" />
+      <p>{burger.name}</p>
+      <p>{burger.price}</p>
+    </div>
+  ))
+
   return (
     <div className="App">
       <nav className="nav">Le bon burger</nav>
 
-      <div className="container">
-        <div className="burger-card">
-          <img
-            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
-            alt="Kawaii Burger"
-          />
-          <p>Kawaii Burger</p>
-          <p>6€90</p>
-        </div>
-        <div className="burger-card">
-          <img
-            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
-            alt="Kawaii Burger"
-          />
-          <p>Kawaii Burger</p>
-          <p>6€90</p>
-        </div>
-        <div className="burger-card">
-          <img
-            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
-            alt="Kawaii Burger"
-          />
-          <p>Kawaii Burger</p>
-          <p>6€90</p>
-        </div>
-        <div className="burger-card">
-          <img
-            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
-            alt="Kawaii Burger"
-          />
-          <p>Kawaii Burger</p>
-          <p>6€90</p>
-        </div>
-        <div className="burger-card">
-          <img
-            src="https://ih1.redbubble.net/image.191031762.5461/flat,800x800,070,f.u2.jpg"
-            alt="Kawaii Burger"
-          />
-          <p>Kawaii Burger</p>
-          <p>6€90</p>
-        </div>
-      </div>
+      <div className="container">{burgerList}</div>
     </div>
   )
 }
