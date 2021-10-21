80a98d8bf0315efbdafb31ede9a28803b230cfd8
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-21 12:51:57 +0200

    Add a new burger from the form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,9 +21,25 @@ import fakeData from "./burgers.json"
 function App() {
   const [burgers, setBurgers] = useState(fakeData)
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  const [image, setImage] = useState("")
+
   const deleteBurger = name => {
     const newBurgers = burgers.filter(burger => burger.name !== name)
     setBurgers(newBurgers)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(event)
+
+    const newBurgers = [...burgers, { name, price, image }]
+    setBurgers(newBurgers)
+
+    setName("")
+    setPrice("")
+    setImage("")
   }
 
   const burgerList = burgers.map(burger => (
@@ -40,6 +56,44 @@ function App() {
       <nav className="nav">Le bon burger</nav>
 
       <div className="container">{burgerList}</div>
+
+      <form className="add-form" onSubmit={handleSubmit}>
+        <div>
+          <label for="name">Nom du votre burger</label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            placeholder="Nom de votre burger"
+          />
+        </div>
+        <div>
+          <label for="price">Prix</label>
+          <br />
+          <input
+            type="text"
+            name="price"
+            placeholder="Prix"
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="image">Lien vers votre image</label>
+          <br />
+          <input
+            type="text"
+            name="image"
+            placeholder="Lien vers votre image"
+            value={image}
+            onChange={e => setImage(e.target.value)}
+          />
+        </div>
+        <br />
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   )
 }
